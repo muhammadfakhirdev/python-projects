1214532304f7879d31065435c7ef7b87847ff381
Muhammad Fakhir <rafiqfakhir2@gmail.com>
2026-06-17 22:15:29 +0500

    Update split calculator.py

--- split calculator.py
+++ split calculator.py
@@ -3,9 +3,10 @@
 rent= int(input("enter rent amount : "))
 food= int(input("enter food amount : "))
 electricity_unit= int(input("enter electricity unit : "))
-electricity_unitcharge= int(input("enter electricity per unit charge ; "))
+electricity_unitcharge= int(input("enter electricity per unit charge : "))
 other= int(input("enter other expenses amount, if there is no other expenses enter 0 : ")) #if there is any other expenses so user can enter it here
+person=int(input("enter the number of persons in which  you want to divide the bill : "))
 electricbill= electricity_unit*electricity_unitcharge
-totalbill= rent+food+electricbill+other
+totalbill= (rent+food+electricbill+other)//person
 
 print(totalbill)
